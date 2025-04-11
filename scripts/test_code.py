0a83b92c5bf677c36a468c11066297037f873248
alisonpezzott <alisonpezzott@gmail.com>
2025-04-11 18:49:50 -0300

    refresh deleted

--- scripts/test_code.py
+++
@@ -1,8 +0,0 @@
-import chardet
-
-path = r"C:\Users\alison.pezzott\Documents\projects\pbi-ci-cd-isv-multi-tenant\src\AdventureWorks.SemanticModel\definition\expressions.tmdl"
-
-with open(path, 'r', encoding='windows-1252') as f:
-    result = f.read()
-
-print(result)
